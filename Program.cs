3f189f80e4f6a9bd39e297f3e4829085159dc09c
André Ribeiro <andrers@ucl.br>
2019-08-23 19:56:53 -0300

    Commit Inicial

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _01_Alo_Mundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "João";
+            Console.WriteLine("Ola Mundo! {0}", nome);
+        }
+    }
+}
